1159351c17b6976e5aa11397e9909ae5bf7ffc81
Rodney Roy Gitonga <rodneyroygitonga@gmail.com>
2025-06-26 12:23:33 +0300

    netlify

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import './index.css'; // Removed this import as styling is handled by App.js
 import App from './App';
-import reportWebVitals from './reportWebVitals'; // For performance monitoring
+// import reportWebVitals from './reportWebVitals'; // Removed as it's causing build issues and not essential for core functionality
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,4 +14,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals(); // Removed the call as well
